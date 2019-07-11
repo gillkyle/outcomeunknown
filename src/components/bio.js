729bb5e444d23829591343564f3fff71077ffc6c
Brad Barrish <brad@bradbarrish.com>
2019-07-10 18:09:51 -0700

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -54,11 +54,10 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
+        Written by <strong>{author}</strong> who lives in Los Angeles with his family.
         {` `}
         <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
+          You can follow along on Twitter
         </a>
       </p>
     </div>
